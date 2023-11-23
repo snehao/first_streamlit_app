c09e759b131b194c95a554a06e732f1e2b553039
snehao <snehaspatil@gmail.com>
2023-11-23 14:56:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Dinerzzzz')
+streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('BB Oatmeal')
