163605105f2ee015679a18c33b262198ce3fabb0
snehao <snehaspatil@gmail.com>
2023-11-23 14:52:51 -0500

    Update streamlit_app.py
    fixed code typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title("My Parents New Healthy Diner")
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('BB Oatmeal')
+streamlit.text('Kale Spinach Smoothie')
+streamlit.text('BB Oatmeal')
+streamlit.text('Hard Boiled Egg')
